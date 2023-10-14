ed721a45147666e9879f3911cb03e0e8bc73cac5
karan <karansawan5050@gmail.com>
2023-10-14 13:19:38 +0530

    css issue 5

--- server.cjs
+++ server.cjs
@@ -19,7 +19,8 @@ app.set("view engine", "ejs");
 app.set('views',__dirname+'/views');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/public", express.static("public", { "extensions": ["css"] }));
+app.use(express.static("public", { "extensions": ["css"] }));
+
 
 app.get("/", function (req, res) {
   res.render("home", { content: homeStartingContent, arrs: arrs });
